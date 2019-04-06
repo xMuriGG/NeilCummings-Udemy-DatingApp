4b3799bb111d247a26dec0a00bde1a4d1dcd7812
xMuriGG <muris.cengic@edu.fit.ba>
2019-04-07 01:03:48 +0200

    Section 6 3rd party components

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,7 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
@@ -17,15 +18,14 @@ export class RegisterComponent implements OnInit {
   register() {
     this.authService.register(this.model).subscribe(
       () => {
-        console.log('registered');
+        this.alertify.success('registered');
       },
       error => {
-        console.log(error);
+        this.alertify.error(error);
       }
     );
   }
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 }
